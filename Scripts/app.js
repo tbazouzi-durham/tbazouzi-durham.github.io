a8e5fb90c5badd88ebb5b96189d856dcce860d71
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2022-01-21 23:28:09 -0500

    java not working on live site

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,37 +1,32 @@
 // IIFE -- Immediately Invoked Function Expression
-// AKA Anonymous Self-Executing Function
+// AKA -- Anonymous Self-Executing Function
 (function()
 {
-    /**
-     * 
-     * 
-     */
     function DisplayHomePage()
     {
         console.log("Home Page");
         let AboutUsButton = document.getElementById("AboutUsButton");
-        
-        // redirect to about page on button click
         AboutUsButton.addEventListener("click", function()
         {
             location.href = "about.html";
         });
 
-        // Step 1: Obtain entry point(s) (insertion point / deletion point) reference
+        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
         let DocumentBody = document.body;
+        let MainContent = document.getElementsByTagName("main")[0];
 
-        // Step 2: Create element(s) to insert
+        // Step 2. create an element(s) to insert
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>`
+        let ArticleParagraph = '<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>'
 
-        // Step 3: COnfigure the new element(s)
+        // Step 3. configure the new element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
         MainParagraph.textContent = "This is the Main Paragraph";
         Article.setAttribute("class", "container");
 
-        // Step 4: Add / Insert the new element(s)
+        // Step 4. Add / Insert the new element
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
@@ -56,6 +51,7 @@
     {
         console.log("Contact Page");
     }
+
 
     // named function option
     function Start()
